14c4a7ca546fa91601f89d18bc1c3effa109335b
RecargasYPagos <alexgomezhernandez49@gmail.com>
2026-06-26 15:25:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Mi Punto Conecta iniciado");
+const CONFIG = {
+    negocio: "Mi Punto Conecta",
+    direccion: "Calle Emiliano Zapata S/N\nTriunfo Agrarista\nOsumacinta, Chiapas",
+    terminal: "MPC-001",
+    comisionRecarga: 5,
+    pin: "1234"
+};
+
+const formulario = document.getElementById("contenedorFormulario");
+const ticket = document.getElementById("ticket");
+
+function generarFolio() {
+
+    const hoy = new Date();
+
+    const fecha =
+        hoy.getFullYear() +
+        String(hoy.getMonth() + 1).padStart(2, "0") +
+        String(hoy.getDate()).padStart(2, "0");
+
+    let consecutivo = localStorage.getItem("folio") || 0;
+
+    consecutivo++;
+
+    localStorage.setItem("folio", consecutivo);
+
+    return "MPC-" + fecha + "-" + String(consecutivo).padStart(3, "0");
+
+}
